18830bbebba05dd5a0275e1fc29665135f99a38e
daniel_sim <daniel_sim@data.gov.sg>
2017-07-03 21:21:28 +0800

    open ext links in new window

--- components/ExternalLinks.vue
+++ components/ExternalLinks.vue
@@ -1,19 +1,19 @@
 <template>
 <ul>
   <li>
-    <a href="https://booking.ambulanceservice.com.sg">Nursing Home Bookings</a>
+    <a target="_blank" href="https://booking.ambulanceservice.com.sg">Nursing Home Bookings</a>
   </li>
   <li>
-    <a href="https://xkjyeah.github.io/certs">Staff Certificates</a>
+    <a target="_blank" href="https://xkjyeah.github.io/certs">Staff Certificates</a>
   </li>
   <li>
-    <a href="https://map.ambulanceservice.com.sg">Ambulance Map</a>
+    <a target="_blank" href="https://map.ambulanceservice.com.sg">Ambulance Map</a>
   </li>
   <li>
-    <a href="http://www.smartrax.com.sg/LoginSG.aspx">Ambulance Map (Smartrax)</a>
+    <a target="_blank" href="http://www.smartrax.com.sg/LoginSG.aspx">Ambulance Map (Smartrax)</a>
   </li>
   <li>
-    <a href="https://xkjyeah.github.io/singapore-postal-codes-ui">Singapore Postal Codes</a>
+    <a target="_blank" href="https://xkjyeah.github.io/singapore-postal-codes-ui">Singapore Postal Codes</a>
   </li>
 </ul>
 </template>
